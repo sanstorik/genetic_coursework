0b39e395239b7e1720cf591b4feeb093f105e967
sanstorik <shvetsovvitaliy06@gmail.com>
2017-06-01 16:02:43 +0300

    created main files (initial commit)

---
+++ app/src/main/java/develop/sanstorik/com/genetic_coursework/GeneticAlgorythm.java
@@ -0,0 +1,8 @@
+package develop.sanstorik.com.genetic_coursework;
+
+public class GeneticAlgorythm {
+    private final int INITIAL_POPULATION_COUNT = 10;
+    private final double ACCURACY = 6.1e-5;
+    private float breedingIndividualCount;
+    private float mutationPossibility;
+}
---
+++ app/src/main/java/develop/sanstorik/com/genetic_coursework/MainActivity.java
@@ -0,0 +1,13 @@
+package develop.sanstorik.com.genetic_coursework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
