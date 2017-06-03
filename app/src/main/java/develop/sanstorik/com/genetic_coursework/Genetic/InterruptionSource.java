4b08a664874e492943e4408606d7faa6ba6a3f3a
VitaliySunstory <shvetsovvitaliy06@gmail.com>
2017-06-03 20:19:29 +0300

    Added multiple interruption sources for algo

---
+++ app/src/main/java/develop/sanstorik/com/genetic_coursework/Genetic/InterruptionSource.java
@@ -0,0 +1,26 @@
+package develop.sanstorik.com.genetic_coursework.Genetic;
+
+public class InterruptionSource {
+    private static final double ACCURACY = 6.1e-5;
+    private static final int MAX_ITERATION = 1000;
+
+    private InterruptionSource(){}
+
+    @FunctionalInterface public interface Interruptible{
+        boolean ended(int iteration, double funcValue);
+    }
+
+    public static Interruptible createIterationSource(final int maxIteration){
+        return (iteration, funcVal)-> iteration > maxIteration || iteration > MAX_ITERATION;
+    }
+
+    public static Interruptible createFunctionMaxValueSource(final double funcValue){
+        return (iteration, funcVal) ->
+                Math.abs(funcValue - funcVal) < ACCURACY || iteration > MAX_ITERATION;
+    }
+
+    public static Interruptible createFunctionMinValueSource(final double funcValue){
+        return (iteration, funcVal) ->
+                Math.abs(funcValue - funcVal) < ACCURACY || iteration > MAX_ITERATION;
+    }
+}
